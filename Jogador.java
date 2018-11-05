08cbe9f61db6db7bec9b1a4ea155b503bc5004a3
raizoor <44624093+raizoor@users.noreply.github.com>
2018-11-04 23:16:04 -0200

    qtd_cartas
    Adicionado a qtd_cartas, variável responsável por controlar o jogador ganhador e jogadores perdedores.

--- Jogador.java
+++ Jogador.java
@@ -1,22 +1,26 @@
 public class Jogador {
 
     private String nome;
+    private int qtd_carta;
 
     public Jogador(String nome) {
         this.nome = nome;
+        this.qtd_carta = 4;
     }
     /**
      * Getters
      */
     public String getNome() { return nome; }
+    public int getQtd_Carta() { return qtd_carta; }
     /**
      * Setters
      */
     public void setNome(String nome) { this.nome = nome; }
+    public void setQtd_Carta(int qtd_carta) { this.qtd_carta = qtd_carta; }
     /**
      * toString
      */
     public String toString () {
-        return "Jogador: " + nome;
+        return "Jogador: " + nome + " Quantidade de cartas:" qtd_carta ;
     }
 }
