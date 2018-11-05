df5b6c214a0ae2527a50b5b3967a7dadcded8bd4
raizoor <44624093+raizoor@users.noreply.github.com>
2018-11-05 00:14:52 -0200

    Ajuste

--- Jogador.java
+++ Jogador.java
@@ -1,26 +1,22 @@
 public class Jogador {
 
     private String nome;
-    private int qtd_carta;
-
+    
     public Jogador(String nome) {
         this.nome = nome;
-        this.qtd_carta = 4;
     }
     /**
      * Getters
      */
     public String getNome() { return nome; }
-    public int getQtd_Carta() { return qtd_carta; }
     /**
      * Setters
      */
     public void setNome(String nome) { this.nome = nome; }
-    public void setQtd_Carta(int qtd_carta) { this.qtd_carta = qtd_carta; }
     /**
      * toString
      */
     public String toString () {
-        return "Jogador: " + nome + " Quantidade de cartas:" qtd_carta ;
+        return "Jogador: " + nome;
     }
 }
