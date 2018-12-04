8f5884d8213de847ec3ed520948baefa80e43185
Lucas Py Becker <lucaspybecker@hotmail.com>
2018-12-04 15:44:15 -0200

    2018-12-04 B

--- Baralho.java
+++ Baralho.java
@@ -2,37 +2,40 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Baralho {
+
     private ArrayList<Carta> cartas;
 
-    public Baralho () {
-        cartas = new ArrayList<Carta>();
+    public Baralho() {
+        cartas = new ArrayList<>();
     }
-    
-    public boolean Adicionar (Carta carta) {
-        return cartas.add(carta);
+
+    public void Adicionar(Carta carta) {
+        cartas.add(carta);
     }
-    
-    public boolean Excluir (Carta carta) {
-        return cartas.remove(carta);
+
+    public void Excluir(Carta carta) {
+        cartas.remove(carta);
     }
-    
-    public Carta getCartaByIndex (int index) {
+
+    public Carta getCartaByIndex(int index) {
         return cartas.get(index);
     }
-    
-    public int getSize () {
+
+    public int getSize() {
         return cartas.size();
     }
-    
-    public void GeraBaralho () {
+
+    public void GeraBaralho() {
         Carta carta;
         String letra = "";
+        boolean superTrunfo;
         int cont = 0;
 
-        String [] nomeMoto = new String [32];
+        String[] nomeMoto = new String[32];
+
         nomeMoto[0] = "Harley Davidson";
         // Continuar os nomes das motos até 32
-        
+
         for (int i = 0; i < 8; i++) {
             switch (i) {
                 case 0: letra = "A"; break;
@@ -43,42 +46,43 @@ public class Baralho {
                 case 5: letra = "F"; break;
                 case 6: letra = "G"; break;
                 case 7: letra = "H"; break;
-                }
+            }
             for (int numero = 1; numero <= 4; numero++) {
-                carta = new Carta(letra, numero, nomeMoto[cont]);
+                superTrunfo = (letra.equals("E") && numero == 4); // Carta E4 será o Super Trunfo
+                carta = new Carta(letra, numero, nomeMoto[cont], superTrunfo);
                 cartas.add(carta);
                 cont++;
             }
         }
     }
-    
-    public void Embaralhar () {
+
+    public void Embaralhar() {
         Carta carta1;
         Carta carta2;
         int rand;
-        
+
         for (int i = 0; i < cartas.size(); i++) {
-            rand = ThreadLocalRandom.current().nextInt(0,32);
+            rand = ThreadLocalRandom.current().nextInt(0, 32);
             carta1 = cartas.get(i);
             carta2 = cartas.get(rand);
             cartas.set(i, carta2);
             cartas.set(rand, carta1);
         }
     }
-    
-    public void Listar () {
+
+    public void Listar() {
         Carta carta;
         for (int i = 0; i < cartas.size(); i++) {
             carta = cartas.get(i);
             System.out.println(carta.toString());
         }
     }
-    
-    public void listarCompacto () { // Só pra debug
+
+    public void listarCompacto() { // Só pra debug
         Carta carta;
         for (int i = 0; i < cartas.size(); i++) {
             carta = cartas.get(i);
-            System.out.printf("[%d] %s%d -> %s%n", (i+1), carta.getLetra(), carta.getNumero(), carta.getNome());
+            System.out.printf("[%d] %s%d -> %s%n", (i + 1), carta.getLetra(), carta.getNumero(), carta.getNome());
         }
     }
 }
